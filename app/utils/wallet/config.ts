dba676e2f74e2f36aaadff2fafbabaaa37be9dcd
Mahdi <mahdighajary@gmail.com>
2024-09-02 16:32:41 +0330

    Add web3modal

---
+++ app/utils/wallet/config.ts
@@ -0,0 +1,27 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { mainnet, optimism, optimismSepolia, sepolia } from 'wagmi/chains'
+
+// Get projectId from https://cloud.walletconnect.com
+// export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+export const projectId = "55df23aa617b4f9c344451b907c660d1"
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+export const metadata = {
+  name: 'AppKit',
+  description: 'AppKit Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia, optimism, optimismSepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+})
