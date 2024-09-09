b9fa6d39a0f0f0ad210ea6278540954c9d78568d
Mahdi <mahdighajary@gmail.com>
2024-09-09 16:05:25 +0330

    Add conflict of interest button

---
+++ app/comparison/card/CoIButton.tsx
@@ -0,0 +1,9 @@
+const ConflictButton = () => {
+  return (
+    <button className="rounded-lg border border-red-500 bg-white px-4 py-6 font-semibold text-red-500 transition-colors duration-300 hover:bg-red-100">
+      Conflict of interest
+    </button>
+  )
+}
+
+export default ConflictButton
