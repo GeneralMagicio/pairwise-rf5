e4355aebaa167e13b566b027c5a305d960187210
Mahdi <mahdighajary@gmail.com>
2024-09-18 14:18:48 +0330

    Small typo

--- app/comparison/ballot/modals/BallotSuccessModal.tsx
+++ app/comparison/ballot/modals/BallotSuccessModal.tsx
@@ -27,7 +27,7 @@ const BallotSuccessModal: React.FC<UnlockBallotProps> = ({ onClick }) => {
           onClick={onClick}
           className="flex w-full items-center justify-center rounded-lg bg-primary px-4 py-3 text-white transition duration-300 ease-in-out hover:bg-red-600"
         >
-          Go to back to the voting app
+          Go back to the voting app
         </button>
       </div>
     </div>
