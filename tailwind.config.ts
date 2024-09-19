b7a24700b023dc483601889bd1bf3861116b2a87
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-09-19 10:11:14 +0100

    fix: format tailwind file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,46 +2,46 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#FF0420",
+        primary: '#FF0420',
         gray: {
-          50: "#FBFCFE",
-          100: "#F2F3F8",
-          200: "#E0E2EB",
-          300: "#F2F4F7",
-          400: "#636779",
-          500: "#98A2B3",
-          600: "#404454",
-          700: "#0F111A",
+          50: '#FBFCFE',
+          100: '#F2F3F8',
+          200: '#E0E2EB',
+          300: '#F2F4F7',
+          400: '#636779',
+          500: '#98A2B3',
+          600: '#404454',
+          700: '#0F111A',
         },
         blue: {
-          foreground: "#3374DB",
-          background: "#D6E4FF",
+          foreground: '#3374DB',
+          background: '#D6E4FF',
         },
         dark: {
-          500: "#05060B",
+          500: '#05060B',
         },
       },
       fontFamily: {
-        inter: ["var(--font-inter)"],
-        mono: ["var(--font-roboto-mono)"],
+        inter: ['var(--font-inter)'],
+        mono: ['var(--font-roboto-mono)'],
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       screens: {
         // small laptops
-        sl: { max: "1400px" },
+        sl: { max: '1400px' },
         // laptops
-        l: { max: "1920px" },
+        l: { max: '1920px' },
       },
     },
   },
