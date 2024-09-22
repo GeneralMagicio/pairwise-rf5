c83fed44403126251e8ab16ca433d90b033f7a0c
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-09-22 15:51:10 +0100

    fix: add expand text only when it's more than 2 lines

--- app/comparison/card/ProjectDescription.tsx
+++ app/comparison/card/ProjectDescription.tsx
@@ -1,8 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from '@/app/styles/Project.module.css';
 
-const ProjectDescription = ({ description }: { description: string }) => {
+interface ProjectDescriptionProps {
+  description: string;
+}
+
+const ProjectDescription: React.FC<ProjectDescriptionProps> = ({ description }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
+  const textRef = useRef<HTMLParagraphElement>(null);
+
+  // Check if the description has more than 3 lines
+  useEffect(() => {
+    if (textRef.current) {
+      const maxHeight = parseInt(getComputedStyle(textRef.current).lineHeight) * 3;
+      if (textRef.current.scrollHeight > maxHeight) {
+        setShowButtons(true);
+      }
+    }
+  }, [description]);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -11,25 +27,26 @@ const ProjectDescription = ({ description }: { description: string }) => {
   return (
     <div className="mb-8 flex flex-col gap-2">
       <p
+        ref={textRef}
         onClick={toggleExpand}
         className={`mb-2 cursor-pointer font-inter text-base font-normal leading-6 text-slate-600 ${
-          !isExpanded ? styles.lineClamp : ''
+          !isExpanded && showButtons ? styles.lineClamp : ''
         }`}
       >
         {description}
       </p>
-      {!isExpanded && (
+      {showButtons && !isExpanded && (
         <span
           onClick={toggleExpand}
-          className="cursor-pointer text-dark-600 hover:underline"
+          className="cursor-pointer font-semibold text-dark-600 hover:underline"
         >
           View more
         </span>
       )}
-      {isExpanded && (
+      {showButtons && isExpanded && (
         <span
           onClick={toggleExpand}
-          className="cursor-pointer text-dark-600 hover:underline"
+          className="cursor-pointer font-semibold text-dark-600 hover:underline"
         >
           View less
         </span>
