4f85cbdfa93b1f40b2c87562546572a4f2af72bb
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-09-25 12:52:33 +0100

    feat: update good rating modal UI

--- app/comparison/card/modals/GoodRatingModal.tsx
+++ app/comparison/card/modals/GoodRatingModal.tsx
@@ -1,40 +1,72 @@
-import React, { FC } from 'react';
-import Image from 'next/image';
+import React, { FC } from "react";
+import Image from "next/image";
+import { StarIcon } from "@/public/assets/icon-components/Star";
 
 interface IGoodRatingModalProps {
   confirm: () => void;
 }
 
-const GoodRatingModal: FC<IGoodRatingModalProps> = ({ confirm }) => {
-  return (
-    <div className="mx-auto rounded-lg bg-white bg-good-rating bg-no-repeat p-6 shadow-lg">
-      <div className="mb-4 flex justify-center">
-        <div className="rounded-full p-2">
-          <Image
-            src="/assets/images/op-character5.svg"
-            alt="Rating Illustration"
-            width={160}
-            height={180}
-          />
-        </div>
+interface IStarRatingProps {
+  color: string;
+  numRated: number;
+}
+
+const StarRating: FC<IStarRatingProps> = ({ color, numRated }) => (
+  <div className="flex gap-1">
+    {[...Array(5)].map((_, index) => (
+      <StarIcon
+        key={index}
+        color={index < numRated ? color : "#D0D5DD"}
+        size={20}
+      />
+    ))}
+  </div>
+);
+
+const ratingOptions = [
+  { color: "#FF1D1D", numRated: 1, label: "No impact on the OP stack" },
+  { color: "#FF8C22", numRated: 2, label: "Low impact on the OP stack" },
+  { color: "#F8CE00", numRated: 3, label: "Medium impact on the OP stack" },
+  { color: "#46C34C", numRated: 4, label: "High impact on the OP stack" },
+  { color: "#479F78", numRated: 5, label: "Very High impact on the OP stack" },
+];
+
+const GoodRatingModal: FC<IGoodRatingModalProps> = ({ confirm }) => (
+  <div className="mx-auto rounded-lg bg-white bg-good-rating bg-no-repeat p-6 shadow-lg">
+    <div className="mb-4 flex justify-center">
+      <div className="rounded-full p-2">
+        <Image
+          src="/assets/images/op-character5.svg"
+          alt="Rating Illustration"
+          width={160}
+          height={180}
+        />
       </div>
-
-      <h2 className="mb-4 text-center text-xl font-semibold text-dark-500">
-        Great choice!
-      </h2>
-
-      <p className="mb-6 text-center text-sm text-gray-400">
-        How would you rate the second project?
-      </p>
-
-      <button
-        onClick={confirm}
-        className="mb-2 w-full rounded-md bg-primary py-2 text-white transition duration-300 hover:bg-red-600"
-      >
-        OK
-      </button>
     </div>
-  );
-};
+
+    <h2 className="mb-6 text-center text-xl font-semibold text-dark-500">
+      How would you rate the second project?
+    </h2>
+
+    <div className="flex flex-col text-gray-400 text-sm mb-8 justify-center gap-6 font-normal font-inter px-2">
+      {ratingOptions.map(({ color, numRated, label }, index) => (
+        <div
+          key={index}
+          className="flex items-center gap-4 md:flex-row flex-col-reverse"
+        >
+          <StarRating color={color} numRated={numRated} />
+          <label htmlFor="good">{label}</label>
+        </div>
+      ))}
+    </div>
+
+    <button
+      onClick={confirm}
+      className="mb-2 w-full rounded-md bg-primary py-2 text-white transition duration-300 hover:bg-red-600"
+    >
+      OK
+    </button>
+  </div>
+);
 
 export default GoodRatingModal;
--- public/assets/icon-components/Star.tsx
+++ public/assets/icon-components/Star.tsx
@@ -1,15 +1,15 @@
-export const StarIcon = () => {
+export const StarIcon = ({ color, size }: { color?: string, size?: number }) => {
   return (
     <svg
-      width="16"
-      height="16"
+      width={size || "16"}
+      height={size || "16"}
       viewBox="0 0 16 16"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <path
         d="M8.00056 12.1733L3.29826 14.8055L4.34848 9.51994L0.39209 5.86121L5.74345 5.22672L8.00056 0.333344L10.2576 5.22672L15.609 5.86121L11.6526 9.51994L12.7028 14.8055L8.00056 12.1733Z"
-        fill="#404454"
+        fill={color || "#404454"}
       />
     </svg>
   );
