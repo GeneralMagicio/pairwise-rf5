d2822b9f7d2390a9950ddd76c8245def7ef9bc51
Meriem-BM <barhoumi.meriem1@gmail.com>
2024-09-29 11:29:13 +0100

    Reapply "fix: update part3 of landing page text"
    This reverts commit 40f7a37ae92d339cd454299fc9a998b0e8f7e678.

--- app/components/TextBlock.tsx
+++ app/components/TextBlock.tsx
@@ -14,7 +14,7 @@ const TextBlock: FC<{
     scale?: number;
   };
 }> = ({ mainText, highlightText, description, highlightImage }) => (
-  <p className="sm:w-[35%]] w-full text-start text-3xl font-bold text-dark-500 xl:text-4xl">
+  <p className="sm:w-[35%]] w-full text-start text-3xl font-bold text-dark-500 xl:text-4xl xl:leading-[3rem]">
     {mainText}{' '}
     <span className="relative inline-block">
       <Image
