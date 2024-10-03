090b91cc9d6f655f6c9bce245399ffddd775b915
Mahdi <mahdighajary@gmail.com>
2024-10-03 17:03:43 +0330

    Revert back baseurl change

--- app/utils/axiosInstance.ts
+++ app/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import Axios, { InternalAxiosRequestConfig } from 'axios';
 
-// export const API_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://pairwise.melodicdays.shop/';
-export const API_URL = 'https://rpgf5-staging-be.pairwise.vote/';
+export const API_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://pairwise.melodicdays.shop/';
+// export const API_URL = 'https://rpgf5-staging-be.pairwise.vote/';
 // export const API_URL = 'http://localhost:7070';
 
 function authRequestInterceptor(config: InternalAxiosRequestConfig) {
